04c4f38eac7999030ea646a5b110bc9ffb85ba8b
arcflowmedia <arcflowmedi.co@gmail.com>
2026-06-03 14:12:44 +0530

    Updated API URL

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxgprYuk5ICscp_mm6vNx65RnB2j_uMO2Y6qTp7f0qCl7BQsucUjPxem_40I39XHbRn/exec";
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwtflJ8a7WU4--PoRV1Ul-_ZBvEOIwL1gfrpbKDeFb1K7pd-7fitYrC3J1Ttl7AxyVx/exec"
